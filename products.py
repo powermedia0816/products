c003d7064684d85584d126d22e07e6fb7aeed7c8
powermedia0816 <jessicayang1004@gmail.com>
2024-05-02 15:18:50 +0800

    products_v3

--- products.py
+++ products.py
@@ -3,7 +3,7 @@ while True:
 	name = input("請輸入商品名稱:")
 	if name == 'q': 
 		break
-	price = input("請輸入商品價格:")
+	price = int(input("請輸入商品價格:"))
 	products.append([name,price])
 print(products)
 
@@ -12,4 +12,4 @@ for p in products: #了解二維清單中儲存的每個東西
 
 with open('products.csv', 'w' ) as f:
 	for p in products:
-		f.write(p[0] + ',' + p[1] + '\n')
+		f.write(p[0] + ',' + str(p[1]) + '\n')
