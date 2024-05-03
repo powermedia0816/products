61de47c25c840891ba5c6ebfeb78690bdbf3bbd4
powermedia0816 <jessicayang1004@gmail.com>
2024-05-03 16:57:02 +0800

    products_v6

--- products.py
+++ products.py
@@ -1,12 +1,19 @@
+import os
+
 # 讀取檔案
 products = []
-with open('products.csv' , 'r' , encoding='utf-8')as f:
-	for line in f:
-		if "商品,價格" in line:
-			continue
-		name, price = line.strip().split(',')
-		products.append([name,price])
-print(products)
+if os.path.isfile('products.csv'): # 確認檔案是否存在
+	print("找到檔案了")
+	with open('products.csv' , 'r' , encoding='utf-8')as f:
+		for line in f:
+		    if "商品,價格" in line:
+			    continue
+		    name, price = line.strip().split(',')
+		    products.append([name,price])
+	print(products)
+else:
+	print("找不到檔案><")
+
 # 使用者輸入
 while True:
 	name = input("請輸入商品名稱:")
@@ -15,6 +22,7 @@ while True:
 	price = int(input("請輸入商品價格:"))
 	products.append([name,price])
 print(products)
+
 #印出購買紀錄
 for p in products: #了解二維清單中儲存的每個東西
 	print(p[0], '的價格是：', p[1])
