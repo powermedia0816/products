7cdf7e190ffa12562467e9aff17f685d847d5e06
powermedia0816 <jessicayang1004@gmail.com>
2024-05-03 15:26:42 +0800

    product_v4

--- products.py
+++ products.py
@@ -10,6 +10,7 @@ print(products)
 for p in products: #了解二維清單中儲存的每個東西
 	print(p[0], '的價格是：', p[1])
 
-with open('products.csv', 'w' ) as f:
+with open('products.csv', 'w' , encoding = 'utf-8') as f:
+	f.write('商品,價格\n')
 	for p in products:
 		f.write(p[0] + ',' + str(p[1]) + '\n')
